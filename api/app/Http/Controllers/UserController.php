cd68b8929c4b08fa6846bd348814db7e0f421541
Jistoria <maestrojgcm@gmail.com>
2024-02-01 18:12:18 -0500

    Ajuste de UserController@register

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -8,51 +8,53 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Spatie\Permission\Models\Role;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     public function userRegister(RegisterRequest $request)
-{
-    try {
-        // Crear un nuevo usuario
-        $new_user = new User;
-        $new_user->name = $request->input('name');
-        $new_user->email = $request->input('email');
-        $new_user->color = $request->input('color');
-        $new_user->password = bcrypt($request->input('password'));
-        $civilRole = Role::where('name', 'Civil')->first();
-        if($civilRole){
-            $new_user->save();
-            $new_user->assignRole($civilRole);
+    {
+        try {
+            // Crear un nuevo usuario
+            $new_user = new User;
+            $new_user->name = $request->input('name');
+            $new_user->email = $request->input('email');
+            $new_user->color = $request->input('color');
+            $new_user->password = bcrypt($request->input('password'));
+            $civilRole = Role::where('name', 'Civil')->first();
+            if($civilRole){
+                $new_user->save();
+                $new_user->assignRole($civilRole);
 
-            // Enviar correo electrónico de verificación
-            $new_user->sendEmailVerificationNotification();
-            Auth::attempt( $request->only('email', 'password'));
-            // autenticar al usuario automáticamente si lo deseas
-            $token = auth()->user()->createToken('token-name')->plainTextToken;
-            $cookie = cookie('cookie_token', $token, 60 * 24);
-            // Redireccionar a la página de inicio o a donde desees después de registrar al usuario
-            return response()->json([
-                                    'success' => true,
-                                    'message' => 'Registrado exitosamente',
-                                    'user'=>auth()->user()],
-                                    Response::HTTP_CREATED)
-                                ->withCookie(($cookie));
+                // Enviar correo electrónico de verificación
+                $new_user->sendEmailVerificationNotification();
+                Auth::attempt( $request->only('email', 'password'));
+                // autenticar al usuario automáticamente si lo deseas
+                $token = auth()->user()->createToken('token-name')->plainTextToken;
+                $cookie = cookie('cookie_token', $token, 60 * 24);
+                // Redireccionar a la página de inicio o a donde desees después de registrar al usuario
+                return response()->json([
+                                        'success' => true,
+                                        'message' => 'Registrado exitosamente',
+                                        'user'=>auth()->user()],
+                                        Response::HTTP_CREATED)
+                                    ->withCookie(($cookie));
+            }
+
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            // Manejar la excepción de validación
+            $errors = $e->validator->errors()->getMessages();
+
+            return response()->json(['success' => false, 'message' => 'Error de validación', 'errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
+        } catch (\Exception $e) {
+            // Manejar otras excepciones
+            return response()->json(['success' => false, 'message' => "Error interno del servidor. $e"], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-    } catch (\Illuminate\Validation\ValidationException $e) {
-        // Manejar la excepción de validación
-        $errors = $e->validator->errors()->getMessages();
-
-        return response()->json(['success' => false, 'message' => 'Error de validación', 'errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
-    } catch (\Exception $e) {
-        // Manejar otras excepciones
-        return response()->json(['success' => false, 'message' => "Error interno del servidor. $e"], Response::HTTP_INTERNAL_SERVER_ERROR);
     }
-}
 
-    public function userLogin (Request $request){
+    public function userLogin (Request $request)
+    {
         // Validación básica de datos
         $request->validate([
             'email' => 'required|email',
@@ -82,6 +84,7 @@ class UserController extends Controller
             return response(['success'=>false, 'message'=>'Credenciales invalidas'], Response::HTTP_UNAUTHORIZED);
         }
     }
+
     public function logout ()
     {
         auth()->user()->tokens()->delete();
@@ -90,23 +93,23 @@ class UserController extends Controller
     }
 
     public function getSessionDetails()
-{
-    $user = auth()->user();
+    {
+        $user = auth()->user();
 
-    if ($user) {
-        // Obtener detalles del usuario, como el tipo y rol
-        $userDetails = [
-            'id' => $user->id,
-            'name' => $user->name,
-            'email' => $user->email,
-            'color' => $user->color,
-            'countNotifications' => $user->unreadNotifications->count(),
-            'role' => $user->getRoleNames()->first(), // Obtener el primer rol del usuario
-        ];
+        if ($user) {
+            // Obtener detalles del usuario, como el tipo y rol
+            $userDetails = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'color' => $user->color,
+                'countNotifications' => $user->unreadNotifications->count(),
+                'role' => $user->getRoleNames()->first(), // Obtener el primer rol del usuario
+            ];
 
-        return response()->json(['user' => $userDetails]);
+            return response()->json(['user' => $userDetails]);
+        }
+
+        return response()->json(['error' => 'Usuario no autenticado'], JsonResponse::HTTP_UNAUTHORIZED);
     }
-
-    return response()->json(['error' => 'Usuario no autenticado'], JsonResponse::HTTP_UNAUTHORIZED);
-}
 }
