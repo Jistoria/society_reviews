9d69cad70bafca47a08aec2bfdfff78aa91c3a01
Jistoria <maestrojgcm@gmail.com>
2024-02-19 14:49:37 -0500

    mejorar middleware
    mejorar ajuste del middleware guest

--- api/app/Http/Middleware/RedirectIfAuthenticated.php
+++ api/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -8,20 +8,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-class RedirectIfAuthenticated extends Middleware
+class RedirectIfAuthenticated
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle($request, Closure $next, ...$guards)
+    public function handle($request, Closure $next, $guard)
     {
         if ($token = $request->cookie('cookie_token')) {
             $request->headers->set('Authorization', 'Bearer ' . $token);
         }
+
             // Si hay un token de autenticación en la solicitud, intenta autenticar al usuario utilizando ese token
-            if (Auth::guard('sanctum')->check()) {
+            if (Auth::guard($guard)->check()) {
                 return response()->json(['success'=>false, 'message'=>'Hay Sesión'],403);
             }
         // Si el usuario no está autenticado, permite que la solicitud continúe sin cambios
