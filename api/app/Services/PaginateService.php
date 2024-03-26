45b5dcd9a994ad58afd4589c8c441ec91040f3c5
Jistoria <maestrojgcm@gmail.com>
2024-03-26 13:54:06 -0500

    Cambio de enfoque
    Ahora se pagina review

--- api/app/Services/PaginateService.php
+++ api/app/Services/PaginateService.php
@@ -8,34 +8,44 @@ use App\Models\Review;
 class PaginateService
 {
     protected $franchiseModel;
-    public function __construct(Franchise $franchise)
+    protected $reviewModel;
+    public function __construct(Franchise $franchise, Review $review)
     {
         $this->franchiseModel = $franchise;
+        $this->reviewModel = $review;
     }
 
     public function paginate($search = null)
     {
-        $query = $this->franchiseModel::with([
-            'reviews' => function ($query) {
-                $query->select('review_id', 'franchise_id', 'content_type_id', 'title_alternative', 'rating_main', 'slug', 'published')
-                        ->where('published', true);
-            },
-            'reviews.contentType:content_type_id,type',
-            'tags:tag_id,name_tag'
-        ])->select('franchise_id', 'title', 'slug', 'franchise_rating','image_url');
-
-        $query->when($search, function ($query, $search) {
+        $query = $this->reviewModel::with('franchise:franchise_id,title,slug,image_url',
+            'franchise.tags:tag_id,name_tag',
+            'contentType:content_type_id,type')
+        ->select('review_id', 'franchise_id', 'content_type_id', 'title_alternative', 'rating_main', 'slug')
+        ->when($search, function ($query, $search) {
             // Agregar condiciones de búsqueda según tus criterios
             $query->where('title', 'like', "%$search%")
                 ->orWhere('title_alternative', 'like', "%$search%");
-        });
-
-        // Franquicias con al menos una revisión publicada
-        $query->whereHas('reviews',function($query){ $query->where('published',true);});
-
+        })
+        ->where('published', true);
         return $query->paginate(10);
     }
+    // $query = $this->franchiseModel::with([
+    //     'reviews' => function ($query) {
+    //         $query->select('review_id', 'franchise_id', 'content_type_id', 'title_alternative', 'rating_main', 'slug', 'published')
+    //                 ->where('published', true);
+    //     },
+    //     'reviews.contentType:content_type_id,type',
+    //     'tags:tag_id,name_tag'
+    // ])->select('franchise_id', 'title', 'slug', 'franchise_rating','image_url');
 
+    // $query->when($search, function ($query, $search) {
+    //     // Agregar condiciones de búsqueda según tus criterios
+    //     $query->where('title', 'like', "%$search%")
+    //         ->orWhere('title_alternative', 'like', "%$search%");
+    // });
+
+    // // Franquicias con al menos una revisión publicada
+    // $query->whereHas('reviews',function($query){ $query->where('published',true);});
 
 
 }
